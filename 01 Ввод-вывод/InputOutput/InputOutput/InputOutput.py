ed5653c6c6702a94b9e87f0711cacb7a5f45225d
Olga Gushchina <olga_guschina@mail.ru>
2023-09-18 11:39:30 +0300

    Добавьте файлы проекта.

---
+++ 01 Ввод-вывод/InputOutput/InputOutput/InputOutput.py
@@ -0,0 +1,14 @@
+﻿print("Пожалуйста, введите, кто ваш питомец?")
+type = input()
+print("Введите имя:")
+name = input()
+print("Введите возраст:")
+age = int(input())
+rem = age % 10
+if rem == 1 :
+    str_end = "год."
+elif rem > 1 and rem < 5:
+    str_end = "года."
+else:
+    str_end = "лет."
+print("Это ", type, "по кличке \"", name, ". Возраст: ", age, str_end)
